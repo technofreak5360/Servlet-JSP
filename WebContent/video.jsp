31fab0539b13eba2b768690e8b9b9645a93c4d69
mmyadav <mintu08.yadav@gmail.com>
2020-04-07 18:55:58 +0530

    server-part

---
+++ WebContent/video.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<% 
+response.setHeader("Cache-Control","no-cache,no-store,must-revalidate" ); // http 1.1 above
+response.setHeader("Paragma","no-cache"); // http 1.0 and below
+response.setHeader("Expires","0"); // proxies
+ if(session.getAttribute("username")==null)
+ {
+	 response.sendRedirect("login.jsp");
+ }
+ 
+%>
+<form action="Logout" method="post">
+<input type="submit" value="Logout">
+</form>
+</body>
+</html>
---
+++ src/com/login/dao/LoginDao.java
@@ -0,0 +1,34 @@
+package com.login.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class LoginDao {
+	String sql = "select * from login where uname=? and pass=?";
+	String url = "jdbc:mysql://localhost:3306/servletlogin";
+	String username = "root";
+	String password = "yadav@123";
+
+	public boolean check(String uname,String pass)
+	{
+				try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, username, password);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, uname);
+			st.setString(2, pass);
+			ResultSet rs = st.executeQuery();
+			if(rs.next())
+			{
+				return true;
+			}
+		} catch ( Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
